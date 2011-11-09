53dbac214bfe4742250d00c6eff9b2f03196227d
Olivier Girardot <ssaboum@gmail.com>
2011-11-09 20:15:46 +0100

    useless settings are useless, + cosmetics and indentation

--- achievements/tasks.py
+++ achievements/tasks.py
@@ -3,4 +3,4 @@ from celery.task import task
 
 @task
 def check_achievement_task(sender, user, key, *args, **kwargs):
-	check_achievement_plain(sender, user, key, *args, **kwargs)
+    check_achievement_plain(sender, user, key, *args, **kwargs)
--- achievements/utils.py
+++ achievements/utils.py
@@ -3,30 +3,30 @@ from achievements.models import Achievement, UserAchievement
 from achievements.signals import achievement_registered, achievement_unlocked
 
 def get_user_score(user):      
-	""" Compute the score of a given user taking into account their Achievement's bonuses"""
-	return UserAchievement.objects.filter(user=user).aggregate(score=Sum('achievement__bonus'))     
+    """ Compute the score of a given user taking into account their Achievement's bonuses"""
+    return UserAchievement.objects.filter(user=user).aggregate(score=Sum('achievement__bonus'))     
 
 def check_achievement_plain(sender, user, key, *args, **kwargs):
-	obj = Achievement.objects.get(key=key)
-	if evaluate_achievement_callback(user, obj, *args, **kwargs):
-		(user_ach, created) = UserAchievement.objects.get_or_create(achievement=obj, user=user)
-		if created:
-			achievement_unlocked.send(sender=sender, user=user, achievement=obj, *args, **kwargs)
-		                                
+    obj = Achievement.objects.get(key=key)
+    if evaluate_achievement_callback(user, obj, *args, **kwargs):
+        (user_ach, created) = UserAchievement.objects.get_or_create(achievement=obj, user=user)
+        if created:
+            achievement_unlocked.send(sender=sender, user=user, achievement=obj, *args, **kwargs)
+                                        
 def evaluate_achievement_callback(user, obj, *args, **kwargs):
-	""" 
-	Evaluate callback of the achievement in order to determine if achievement has been
-	unlocked by user.
-	"""            
-	achievement = get_callback_object(obj.callback)
-	# call the evaluate fonction :
-	return achievement().evaluate(user, *args, **kwargs)
+    """ 
+    Evaluate callback of the achievement in order to determine if achievement has been
+    unlocked by user.
+    """            
+    achievement = get_callback_object(obj.callback)
+    # call the evaluate fonction :
+    return achievement().evaluate(user, *args, **kwargs)
                                                    
 def construct_callback(obj): 
-	return "%s.%s" % (obj.__module__, obj.__name__)
+    return "%s.%s" % (obj.__module__, obj.__name__)
 
 def get_callback_object(ref):
-	module = ".".join(ref.split(".")[:-1])
-	class_name = ref.split(".")[-1]    
-	m = importlib.import_module(module)
-	return eval('m.%s' % class_name)
+    module = ".".join(ref.split(".")[:-1])
+    class_name = ref.split(".")[-1]    
+    m = importlib.import_module(module)
+    return eval('m.%s' % class_name)
