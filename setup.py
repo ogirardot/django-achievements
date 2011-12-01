ace8340667e6db9f27f3268203aa58575e89f74b
Olivier Girardot <ssaboum@gmail.com>
2011-12-01 09:27:20 +0100

    adding dependencies in setup.py fixes #3

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 setup(
     name = "django-achievements",
     packages = ["achievements", "achievements.migrations", "achievements.templatetags"],
-    version = "0.0.6",
+    version = "0.0.7",
     description = "Django application to deal with achievements and rewards unlocked by users",
     author = "Olivier Girardot",
     author_email = "ssaboum@gmail.com",
     url = "https://github.com/ssaboum/django-achievements",
+    install_requires = ["importlib>=1.0.2", "django-appconf>=0.4.1"],
     classifiers = [
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
