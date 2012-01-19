66608fe53196f6960ab5440bbc9ac323dacb20f1
Chris <chris@imendo.at>
2012-01-19 11:18:18 +0100

    pep8

--- achievements/engine.py
+++ achievements/engine.py
@@ -1,41 +1,42 @@
-from django.conf import settings                                                
-from achievements.models import Achievement, UserAchievement                    
+from django.conf import settings
+from achievements.models import Achievement
 from achievements.utils import construct_callback, check_achievement_plain
-from achievements.signals import achievement_registered, achievement_unlocked
-            
+from achievements.signals import achievement_registered
+
+
 class AchievementEngine(object):
-    def register_achievement(self, cls):  
-        """ Register a new achievement into the engine and database"""    
+    def register_achievement(self, cls):
+        """ Register a new achievement into the engine and database"""
         (obj, created) = Achievement.objects.get_or_create(key=cls.key,  defaults={
-            'name' : cls.name,
-            'description' : cls.description, 
-            'bonus' : cls.bonus, 
+            'name': cls.name,
+            'description': cls.description,
+            'bonus': cls.bonus,
             'callback': construct_callback(cls)})
         if not created:
             # update the object if key didn't change :
             obj.name = cls.name
             obj.description = cls.description
             obj.bonus = cls.bonus
-            obj.callback = construct_callback(cls) 
+            obj.callback = construct_callback(cls)
             obj.save()
-        achievement_registered.send(sender=self, achievement_class=cls)      
-                   
+        achievement_registered.send(sender=self, achievement_class=cls)
+
     def check_achievement(self, user, key, *args, **kwargs):
-        """ 
-        Check synchronously or asynchronously if according to a specific context 
+        """
+        Check synchronously or asynchronously if according to a specific context
         an achievement has been unlocked
         """
-        if user and user.is_authenticated(): 
-            if settings.ACHIEVEMENT_USE_CELERY: 
+        if user and user.is_authenticated():
+            if settings.ACHIEVEMENT_USE_CELERY:
                 # do not try to import if celery is not defined
                 from achievements.tasks import check_achievement_task
                 check_achievement_task.delay(self, user, key, *args, **kwargs)
-            else:     
+            else:
                 check_achievement_plain(self, user, key, *args, **kwargs)
         else:
             print "trying to check an achievement for an un-logged user"
-                                                     
-# create the engine    
+
+
+# create the engine
 engine = AchievementEngine()
 
-        
--- achievements/tasks.py
+++ achievements/tasks.py
@@ -1,6 +1,8 @@
 from achievements.utils import check_achievement_plain
-from celery.task import task  
+from celery.task import task
+
 
 @task
 def check_achievement_task(sender, user, key, *args, **kwargs):
     check_achievement_plain(sender, user, key, *args, **kwargs)
+
